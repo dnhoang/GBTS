7f1cf08e569d8001c26427b4ef0e8470d13ed3ab
HoangDN <dongochoangvn@gmail.com>
2016-10-24 00:25:06 +0700

    Complete Promotion Function Coding Income Report Function: Integrate Chart

---
+++ Web_App/Main_Web/Green_Bus_Ticket_System/Green_Bus_Ticket_System/Areas/Manager/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Green_Bus_Ticket_System.Areas.Manager.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Manager/Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
